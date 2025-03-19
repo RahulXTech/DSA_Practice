ee373b47f5900c11a8d6750589c5de82323efcc0
rahulkumarclg <rahul.code2024@gmail.com>
2025-03-19 20:28:45 +0530

    Today, I solved the problem

--- SortingAlgo/SortingArr.cpp
+++ SortingAlgo/SortingArr.cpp
@@ -1,18 +1,13 @@
 #include<iostream>
 #include<string>
-
 using namespace std;
-
 int main(){
-
 int arr[] = {2,4,5,1,7,6};
-
 for(int i =0; i<6; i++){
-    for()
+    for(int j = j+i; j<6; j++){
+
+    }
 }
 
-
-
-
-    return 0;
+return 0;
 }
