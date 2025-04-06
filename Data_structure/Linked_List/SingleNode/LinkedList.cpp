56f4768d188f989a163908f26e94836b16bfa5bb
rahulkumarclg <rahul.code2024@gmail.com>
2025-04-06 16:52:23 +0530

    learn linked list problem

---
+++ Data_structure/Linked_List/SingleNode/LinkedList.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class NODE{
+    public:
+    int data;
+    NODE *next;
+
+    NODE(int val){
+        data = val;
+        next = NULL;
+    } 
+};
+int main(){
+//Creating dynamic object
+NODE *HEAD;
+HEAD = new NODE(5);
+
+    return 0;
+}
