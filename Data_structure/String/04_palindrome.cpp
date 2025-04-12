6c52126b2eda7b15d52e0083c75cf2dd1e699e90
rahulkumarclg <rahul.code2024@gmail.com>
2025-04-12 14:08:03 +0530

    palindrome restrict mode problem

--- Data_structure/String/04_palindrome.cpp
+++ Data_structure/String/04_palindrome.cpp
@@ -1,22 +1,35 @@
 #include<iostream>
 #include<string>
+#include<typeinfo>
 
 using namespace std;
 
 int main(){
 //Check if string is a palindrome or not. like racecar
+//Check the strict mode data types.
 cout<<"Please enter the string : ";
 string name;
 cin>>name;
 string values = name;
 
-string name1 = "racecar";
 
 int start = 0;
 int n = values.length();
 int end = n-1;
 
 while(start < end){
+    if(values[start]){
+
+    }
+    if(typeid(values[start]).name() != typeid(string).name()){
+        start++;
+        continue;
+    };
+    if(typeid(values[end]).name() != typeid(string).name()){
+        end--;
+        continue;
+    };
+
     swap(values[start], values[end]);
     start++;
     end--;
@@ -24,7 +37,6 @@ while(start < end){
 if(name == values){
     cout<<"True, It's palindrome ";
 }else cout<<"False, It's not palindrome";
-
 
     return 0;
 }
