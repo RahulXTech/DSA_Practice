b6d34140c4e02dcb3a235f75b35887d5c503bea1
rahulkumarclg <rahul.code2024@gmail.com>
2025-04-13 16:54:34 +0530

    Remove all occurrences substring problme

---
+++ Data_structure/String/PermutationInString.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    //STL function
+   //string.find();
+   //string.erase(strting_point, end_point);
+   string s = "daabcbaabcbc";
+   string part = "abc";
+
+   while (s.find(part) < s.length() && s.length() > 0)
+   {
+    cout<<"this is the answer : "<< s.erase(s.find(part), s.length()) <<endl;
+   }
+    return 0;
+}
