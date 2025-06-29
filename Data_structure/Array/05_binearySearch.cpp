de2865c28f19bf71da6e213fc81b4d8880767970
rahulkumarclg <rahul.code2024@gmail.com>
2025-06-29 14:17:11 +0530

    bineary search

---
+++ Data_structure/Array/05_binearySearch.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int BinearSearch(vector<int> arr, int target){
+    int left = 0;
+    int right = arr.size()-1;
+while(left<=right){
+    int mid = (right + left)/2;
+    if(arr[mid]==target){
+        cout<<"Your target is available on index : "<<mid<<endl;
+        return true;
+    }
+    if ( target > arr[mid] ){
+        mid++;
+        left=mid;
+
+    }
+    if(target<arr[mid]){
+        mid--;
+        right=mid;
+
+    }
+}
+return false;
+};
+int main(){
+
+vector<int> arrs = {2,3,4,5,6,7,8,9,10,12};
+
+cout<<BinearSearch(arrs, 12);
+// cout<<"your code is working";
+    return 0;
+}
