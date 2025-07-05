263b7d211aa2fad470985972db77e79f35373fc0
rahulkumarclg <rahul.code2024@gmail.com>
2025-07-05 18:03:50 +0530

    recursion

--- Recursion/03_SumOfNNum.cpp
+++ Recursion/03_SumOfNNum.cpp
@@ -2,17 +2,12 @@
 #include<string>
 using namespace std;
 
-
 int SumOfNNum(int n){
 if(n == 0) return 1;
 
     return n + SumOfNNum(n - 1);
 };
-
-
-
 int main(){
-
 cout<<SumOfNNum(4);
-    return 0;
+return 0;
 }
