31e9b73b62d5aaa5caffe29aa532c8d95b6230a5
rahulkumarclg <rahul.code2024@gmail.com>
2025-07-15 14:17:19 +0530

    Leetcode reverse words string III problem

--- LeetCode/02_String/10_GoalParserInterpretation.cpp
+++ LeetCode/02_String/10_GoalParserInterpretation.cpp
@@ -4,17 +4,16 @@ using namespace std;
 string GoalParserString(string command){
     string result = "";
     for(int i=0; i<command.length(); i++){
-        int sample = command[i] + command[i+1];
+        string sample = string() + command[i] + command[i+1];
         if(sample == "()"){
             result+='o';
-            cout<<"yes open and close breaket is avlaible";
         }
         if(command[i]!='(' && command[i]!=')') result+=command[i];
     }
     return result;
 };
 int main(){
-    string inp = "G()od";
+    string inp = "G()od_M()rning";
     cout<<GoalParserString(inp);
     return 0;
 };
