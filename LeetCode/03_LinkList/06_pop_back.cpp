feb8a060c8dc88b9b9144df3d0a74edb085dc34a
rahulkumarclg <rahul.code2024@gmail.com>
2025-07-25 14:03:13 +0530

    POP Back Opration in LinkedList

---
+++ LeetCode/03_LinkList/06_pop_back.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Node
+{
+    public:
+        int data;
+        Node* next;
+
+        Node(int val){
+            data = val;
+            next = NULL;
+        }
+};
+class List{
+    public:
+        Node* head;
+        Node* tail;
+
+        List(){
+            head = tail = NULL;
+        }
+        void push_back(int val){
+            Node* newNode = new Node(val);
+            if(head == NULL){
+                head = tail = newNode;
+            }else{
+                tail->next= newNode;
+                tail = newNode;
+            }
+        }
+        void printLL(){
+            Node* temp = head;
+            while(temp != NULL){
+                cout<<temp->data<<" -> ";
+                temp = temp->next;
+            }
+            cout<<"NULL";
+        }
+
+        void pop_back(){
+            if(head == NULL){
+                cout<<"sorry your LL is Empty";
+            }
+
+            Node* temp = head;
+            while(temp->next != tail){
+                temp = temp->next;
+            }
+
+            temp->next = NULL;
+            delete tail;
+            tail = temp;
+
+        }
+};
+int main(){
+    List ll;
+    ll.push_back(34);
+    ll.push_back(21);
+    ll.push_back(93);
+    ll.push_back(43);
+    ll.pop_back();
+    ll.printLL();
+    return 0;
+};
