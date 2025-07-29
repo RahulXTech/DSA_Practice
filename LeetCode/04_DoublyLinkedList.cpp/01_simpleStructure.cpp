8c2cfde569ac1ebcfb5091edeb9873e36ebe10e5
rahulkumarclg <rahul.code2024@gmail.com>
2025-07-29 12:04:18 +0530

    Doubly linkedlist Done

--- LeetCode/04_DoublyLinkedList.cpp/01_simpleStructure.cpp
+++ LeetCode/04_DoublyLinkedList.cpp/01_simpleStructure.cpp
@@ -43,22 +43,50 @@ class doublyLL{
     void push_back(int val){
         Node* newNode = new Node(val);
         if(head == NULL){
-            head = tail = NULL;
+            head = tail = newNode;
         }else{
             tail->next = newNode;
             newNode->prev = tail;
             tail = newNode;
         }
     }
+    void pop_front(){
+        if(head == NULL){
+            cout<<"Linkedlist is empty";
+            return;
+        }
+
+        Node* tempHead = head;
+        head = head->next;
+        if(head!=NULL){
+            head->prev = NULL;
+        }
+        tempHead->next = NULL;
+        delete tempHead;
+    }
+    void pop_back(){
+        Node* temp = head;
+        if(head == NULL){
+            cout<<"DLL is null";
+        }
+        tail =tail->prev;
+        if(tail!=NULL){
+        tail->next = NULL;
+        }
+        temp->prev = NULL;
+        delete tail;
+    }
 };
 
 int main(){
     doublyLL DLL;
-    DLL.push_front(43);
-    DLL.push_front(54);
-    DLL.push_front(65);
     DLL.push_back(22);
+    DLL.push_back(32);
+    DLL.push_back(83);
 
+    DLL.pop_front();
+    DLL.pop_back();
+    DLL.pop_back();
     DLL.printLL();
     return 0;
 };
