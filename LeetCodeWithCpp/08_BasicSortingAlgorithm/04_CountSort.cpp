d826319e32e7003d9efd5a578777ac0184675887
rahulkumarclg <rahul.code2024@gmail.com>
2025-09-01 11:41:29 +0530

    Count sort algorithm

--- LeetCodeWithCpp/08_BasicSortingAlgorithm/04_CountSort.cpp
+++ LeetCodeWithCpp/08_BasicSortingAlgorithm/04_CountSort.cpp
@@ -1,30 +1,47 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <climits>
 using namespace std;
-void countingSort(vector<int>arr, int n);
-void printElements(vector<int>arr, int n);
 
-
-void printElements(vector<int>arr, int n){
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
+void printElements(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
     }
-};
-
-void countingSort(vector<int>arr, int n){
-    for(int i=0; i<n; i++){
-
-    }
-
-
-    printElements(arr,n);
+    cout << endl;
 }
 
+void countingSort(int arr[], int n){
+    int minVal = INT_MAX, maxVal = INT_MIN;
 
+    // Find min and max
+    for(int i = 0; i < n; i++){
+        minVal = min(minVal, arr[i]);
+        maxVal = max(maxVal, arr[i]);
+    }
+
+    int range = maxVal - minVal + 1;
+    int* freq = new int[range](); // dynamic array, initialized to 0
+
+    // Count frequency
+    for(int i = 0; i < n; i++){
+        freq[arr[i] - minVal]++;
+    }
+
+    // Reconstruct sorted array
+    for(int i = minVal, j = 0; i <= maxVal; i++){
+        while(freq[i - minVal] > 0){
+            arr[j++] = i;
+            freq[i - minVal]--;
+        }
+    }
+
+    delete[] freq; // free memory
+    printElements(arr, n);
+}
 int main(){
-vector<int>arr = {4,3,5,2,2,1,0,3,5,7,9};
-int n = arr.size();
-countingSort(arr, n);
+    int arr[] = {4, 3, 5, 2, 2, 1, 0, 3, 5, 7, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-return 0;
+    countingSort(arr, n);
+
+    return 0;
 }
