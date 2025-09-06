768a8bcbc3faad0f4fd1726a8db64fd6a6219426
rahulkumarclg <rahul.code2024@gmail.com>
2025-09-06 12:17:36 +0530

    Bit Manuplations

---
+++ BitManuplation/bitwiseOperators.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //Bit manuplation 
+    // Bitwise oprerators
+    cout<<"AND Opration : 3&5 = "<<(3&5)<<endl;
+    cout<<"OR Opration : 3|5 = "<<(3|5)<<endl;
+    cout<<"XOR Opration : 3^5 = "<<(3^5)<<endl;
+    
+    return 0;
+} 
