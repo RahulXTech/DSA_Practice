24e8ccde2fc06d08b6013453d4dadf041beee613
rahulkumarclg <rahul.code2024@gmail.com>
2025-09-22 14:32:38 +0530

    sudoku problem solved using backtracking

--- LeetCodeWithCpp/12_backTraking/06_gridWay.cpp
+++ LeetCodeWithCpp/12_backTraking/06_gridWay.cpp
@@ -7,11 +7,11 @@ int gridWays(int r, int c, int n, int m, string ans){
     if(r==n-1 && c==m-1){
         cout<<ans<<" "<<endl;
         return 1;
-    }  
+    }
     if(r>=n || c>=m) return 0;
-    //right
+    //right..........
     int val2 = gridWays(r, c+1, n, m, ans+'R');
-    //down
+    //down...........
     int val1 = gridWays(r+1, c, n, m, ans + 'L');
 
     return val1+val2;
