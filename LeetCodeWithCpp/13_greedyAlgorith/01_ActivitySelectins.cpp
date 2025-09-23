29508d858ad4cc3dc58f4c21e2a91cc8029fa297
rahulkumarclg <rahul.code2024@gmail.com>
2025-09-23 22:47:25 +0530

    Track the activity problem

---
+++ LeetCodeWithCpp/13_greedyAlgorith/01_ActivitySelectins.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int maxActivitySelection(vector<int>start, vector<int>end){
+    int ans = 1;
+    cout<<"Selecting activity : A0"<<endl;
+    int endPoint = end[0];
+
+    for(int i=0; i<start.size(); i++){
+        if(start[i]>=endPoint){
+            cout<<"Selecting activity : A"<<i<<endl;
+            ans++;
+            endPoint = end[i];
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int>start = {1,3,0,5,8,5};
+    vector<int>end = {2,4,5,6,7,9,9};
+
+    cout<<"Total Activitye is : "<<maxActivitySelection(start,end);
+    
+    return 0;
+}
