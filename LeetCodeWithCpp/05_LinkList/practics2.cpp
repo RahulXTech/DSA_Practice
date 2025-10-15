2c5a5967b67a1da1091ff1a0b09b6374861c0525
RahulXTech <rahul.code2024@gmail.com>
2025-10-15 22:10:03 +0530

    linked list practics

--- LeetCodeWithCpp/05_LinkList/practics2.cpp
+++ LeetCodeWithCpp/05_LinkList/practics2.cpp
@@ -11,49 +11,61 @@ class Node{
         this->next = NULL;
     }
 };
+
+
+
 class List{
     public:
         Node* head;
         Node* tail;
-    List(){
-        head = tail = NULL;
-    }
-    void push_front(int data){
-        Node* newNode = new Node(data);
-        if(head == NULL){
-            head = tail = newNode;
-        }else{
-            newNode->next = head;
-            head = newNode;
+        List(){
+            head=tail=NULL;
         }
-    }
+
+    void push_front(int data){
+        Node* newData = new Node(data);
+        if(head == NULL){
+            head = tail = newData;
+        }else{
+            newData->next = head;
+            head = newData;
+        }
+    };
 
     void push_back(int data){
-        Node* newNode = new Node(data);
+        Node* newData = new Node(data);
         if(head == NULL){
-            head = tail = newNode;
+            head = tail = newData;
         }else{
-            tail->next = newNode;
-            tail = newNode;
+            tail->next = newData;
+            tail = newData;
         }
     }
-    void printLL(){
+    void printNodes(){
         Node* temp = head;
         while(temp!=NULL){
-            cout<<temp->data<<" -> ";
+            cout<<temp->data<<"->";
             temp = temp->next;
         }
         cout<<"NULL"<<endl;
     }
 };
+
 int main(){
     List l1;
+    l1.push_front(4);
+    l1.push_front(6);
     l1.push_front(5);
-    l1.push_front(7);
     l1.push_front(8);
-    l1.push_front(2);
+    l1.push_front(4);
 
-    l1.printLL();
+    l1.push_back(5);
+    l1.push_back(0);
+    l1.push_back(1);
+    l1.push_back(2);
+
+
+    l1.printNodes();
 
     return 0;
 }
