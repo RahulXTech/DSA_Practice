eea41b99dcb87e5978efce026ac67b8a89b18f8f
RahulXTech <rahul.code2024@gmail.com>
2025-10-16 21:46:38 +0530

    doubly linked list practics

---
+++ LeetCodeWithCpp/05_LinkList/13_doublyLinkedList.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* prev;
+        Node(int data){
+            this->data = data;
+            next = NULL;
+            prev = NULL;
+        }
+};
+class List{
+    public:
+        
+};
+int main(){
+
+
+    
+    return 0;
+}
